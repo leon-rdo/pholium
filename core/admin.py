b06b659c0d94f3a3ce56e07223ad761595a0c2f2
Leonardo Costa <leonardoabreu1212@gmail.com>
2025-09-14 16:50:03 -0300

    feat: add Tag model

--- core/admin.py
+++ core/admin.py
@@ -1,10 +1,10 @@
 from django.contrib import admin
-# from .models import Tag
+from .models import Tag
 
 
-# @admin.register(Tag)
-# class TagAdmin(admin.ModelAdmin):
-#     list_display = ("name", "slug", "created_at", "updated_at")
-#     search_fields = ("name", "slug")
-#     prepopulated_fields = {"slug": ("name",)}
-#     readonly_fields = ("created_at", "updated_at")
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    list_display = ("name", "slug", "language", "created_at", "updated_at")
+    search_fields = ("name", "slug", "language")
+    prepopulated_fields = {"slug": ("name",)}
+    readonly_fields = ("created_at", "updated_at")
--- core/models.py
+++ core/models.py
@@ -10,3 +10,27 @@ class TimeStamped(models.Model):
 
     class Meta:
         abstract = True
+
+
+class Tag(TimeStamped):
+    name = models.CharField(_("Name"), max_length=50, unique=True)
+    slug = models.SlugField(_("Slug"), max_length=60, unique=True, blank=True)
+    language = models.CharField(
+        _("Language"),
+        max_length=5,
+        default="en-us",
+        help_text=_("e.g., 'en-us', 'pt-br'"),
+    )
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.name)
+        return super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = _("Tag")
+        verbose_name_plural = _("Tags")
+        ordering = ["name"]
