00a7cebf65257abaac9781f7cd8ed918ccd91574
Leonardo Costa <leonardoabreu1212@gmail.com>
2025-09-21 18:12:22 -0300

    feat: add django-jazzmin for beautiful admin panel

--- pholium/settings.py
+++ pholium/settings.py
@@ -43,6 +43,7 @@ ALLOWED_HOSTS = _csv_env("ALLOWED_HOSTS")
 # Application definition
 
 INSTALLED_APPS = [
+    "jazzmin",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
