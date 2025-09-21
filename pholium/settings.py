98e9265c11db67b0c545631a691fa1e410439b94
Leonardo Costa <leonardoabreu1212@gmail.com>
2025-09-21 18:32:30 -0300

    fix: define only used database engine

--- pholium/settings.py
+++ pholium/settings.py
@@ -103,7 +103,9 @@ WSGI_APPLICATION = "pholium.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-DATABASES = {
+DB_ENGINE = os.getenv("DB_ENGINE", "sqlite")
+
+DB_PROFILES = {
     "sqlite": {
         "ENGINE": "django.db.backends.sqlite3",
         "NAME": BASE_DIR / "db.sqlite3",
@@ -125,7 +127,8 @@ DATABASES = {
         "PORT": os.getenv("DB_PORT", "3306"),
     },
 }
-DATABASES["default"] = DATABASES[os.getenv("DB_ENGINE", "sqlite")]
+
+DATABASES = {"default": DB_PROFILES[DB_ENGINE]}
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
