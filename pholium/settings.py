2d249cb8910132715ee8126a25eb82b601b4072a
Leonardo Costa <leonardoabreu1212@gmail.com>
2025-09-28 14:10:50 -0300

    feat: add configs for media and static files

--- pholium/settings.py
+++ pholium/settings.py
@@ -198,6 +198,7 @@ STATIC_URL = "static/"
 
 # Media files
 MEDIA_URL = os.getenv("MEDIA_URL", "/media/")
+MEDIA_ROOT = BASE_DIR / "media"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
