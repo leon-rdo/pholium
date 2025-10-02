cc3846fe45c67af55b7c13f6a004c2d6d0201dcd
Leonardo Costa <leonardoabreu1212@gmail.com>
2025-10-01 22:45:47 -0300

    chore: redirect empty route to api root

--- pholium/urls.py
+++ pholium/urls.py
@@ -19,11 +19,13 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("i18n/", include("django.conf.urls.i18n")),
     path("api/", include("core.urls", namespace="core")),
+    path("", RedirectView.as_view(url="/api/")),
 ]
 
 if settings.DEBUG:
