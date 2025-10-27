e5321e06120dee0ae07a7ee3727c6561030b647d
Leonardo Costa <leonardoabreu1212@gmail.com>
2025-10-27 13:37:30 -0300

    fix: add ignore when import fail at AutoFlexFieldsSerializer

--- core/utils/auto_flex_fields_serializer.py
+++ core/utils/auto_flex_fields_serializer.py
@@ -17,7 +17,15 @@ class AutoFlexFieldsSerializer(FlexFieldsModelSerializer):
             if field.is_relation and field.related_model:
                 related_model = field.related_model
                 serializer_name = f"{related_model.__name__}Serializer"
-                serializer_path = f"{related_model._meta.app_label}.{serializer_name}"
+                serializer_path = (
+                    f"{related_model._meta.app_label}.serializers.{serializer_name}"
+                )
                 is_many = field.many_to_many or field.one_to_many
-                expandable[field.name] = (serializer_path, {"many": is_many})
+                try:
+                    from django.utils.module_loading import import_string
+
+                    import_string(serializer_path)
+                    expandable[field.name] = (serializer_path, {"many": is_many})
+                except ImportError:
+                    pass
         return expandable
